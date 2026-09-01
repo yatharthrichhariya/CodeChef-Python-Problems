6a2696be0042049b4008704c6869894b2f89021d
Yatharth Richhariya <yatharthrichhariya@gmail.com>
2026-09-01 23:37:21 +0530

    ✅ FOODBAL - Rating 215 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-01

--- codechef/easy/FOODBAL---Rating-215/solution.py
+++ codechef/easy/FOODBAL---Rating-215/solution.py
@@ -1,7 +1,8 @@
-F1,P1,F2,P2=map(int,input().split())
-if abs(F1-P1) < abs(F2-P2):
+F1, P1, F2, P2 = map(int, input().split())
+
+if abs(F1 - P1) < abs(F2 - P2):
     print("First")
-elif abs(F1-P1) > abs(F2-P2):
+elif abs(F1 - P1) > abs(F2 - P2):
     print("Second")
 else:
-    print('Both')
+    print("Both")
