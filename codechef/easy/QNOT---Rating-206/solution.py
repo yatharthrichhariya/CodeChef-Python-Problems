34360ac0f1a8972da3907fc08a26f9a3dfec07d9
Yatharth Richhariya <yatharthrichhariya@gmail.com>
2026-09-01 23:03:26 +0530

    ✅ QNOT - Rating 206 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/easy/QNOT---Rating-206/solution.py
+++ codechef/easy/QNOT---Rating-206/solution.py
@@ -1,5 +1,6 @@
-N,X,Y=map(int,input().split())
-if N>=2*X and N>=2*Y:
-    print("Yes")
+N, X, Y = map(int, input().split())
+
+if N >= 2 * X and N >= 2 * Y:
+    print("YES")
 else:
-    print("No")
+    print("NO")
